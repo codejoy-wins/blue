c2bb0e0b26efcf8e16495476c683188923a9abee
Max <mjann@ucsc.edu>
2019-07-06 15:28:40 -0700

    codejoy

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 function generator(){
     console.log("generator");
-    console.log(Math.floor(Math.random(0,1)*5));
-    return Math.floor(Math.random(0,1)*5);
+    let answer = Math.floor(Math.random(0,1)*5);
+    console.log(`ans is ${answer}`);
+    if(answer === 0){
+        console.log("ans is 0!");
+        document.getElementById("magic").innerHTML=`
+        <img src="0.jpg" alt="" class="widify">
+        `
+    }
+    if(answer === 1){
+        console.log("ans is 1!");
+        document.getElementById("magic").innerHTML=`
+        <img src="sunset.jpg" alt="" class="widify">
+        `
+    }
+    if(answer > 2){
+        console.log("ans is >2");
+        document.getElementById("magic").innerHTML=`
+        <img src="underwater.jpg" alt="" class="widify">
+        `
+    }
+    return answer;
 }
