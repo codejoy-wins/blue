cffdd280d6cef0aba957c522094c59611c0e92d2
Max <mjann@ucsc.edu>
2019-07-07 09:28:32 -0700

    more pix"

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 function generator(){
     console.log("generator");
-    let answer = Math.floor(Math.random(0,1)*5);
+    let answer = Math.floor(Math.random(0,1)*7);
     console.log(`ans is ${answer}`);
     if(answer === 0){
-        console.log("ans is 0!");
         document.getElementById("magic").innerHTML=`
         <img src="0.jpg" alt="" class="widify">
         `
     }
     if(answer === 1){
-        console.log("ans is 1!");
         document.getElementById("magic").innerHTML=`
         <img src="sunset.jpg" alt="" class="widify">
         `
     }
-    if(answer > 2){
-        console.log("ans is >2");
+    if(answer === 2){
         document.getElementById("magic").innerHTML=`
         <img src="underwater.jpg" alt="" class="widify">
         `
     }
-    if(answer === 2){
-        console.log("ans is 2");
+    if(answer === 3){
         document.getElementById("magic").innerHTML=`
-        <img src="beach.jpeg" alt="" class="widify">
+        <img src="oceanlife.webp" alt="" class="widify">
+        `
+    }
+    if(answer === 4){
+        document.getElementById("magic").innerHTML=`
+        <img src="octopus.jpg" alt="" class="widify">
+        `
+    }
+    if(answer === 5){
+        document.getElementById("magic").innerHTML=`
+        <img src="colors.jpg" alt="" class="widify">
+        `
+    }
+    if(answer === 6){
+        document.getElementById("magic").innerHTML=`
+        <img src="wow.jpg" alt="" class="widify">
         `
     }
     return answer;
